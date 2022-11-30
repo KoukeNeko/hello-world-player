def60c3e1fe0ac3d52bebd153f5e7271e2a75d53
Violet's Foxes <111033412+KoukeNeko@users.noreply.github.com>
2022-11-30 23:43:23 +0800

    try to enable vibrancy

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App" role="main">
-      <article className="App-article">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3 className="text-3xl font-bold underline">Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-      </article>
+    <div className=" App hero min-h-screen bg-transparent">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
