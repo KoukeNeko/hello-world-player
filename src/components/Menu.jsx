c99ed3fac00f1d31f24dcc4abaff9dcd8f06c441
Violet's Foxes <111033412+VioletsFoxes@users.noreply.github.com>
2022-12-02 11:40:20 +0800

    add menu items

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faGear, faHouse, faHeart, faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faGear, faHouse, faMusic, faHeart, faBookmark, faBarsStaggered, faList } from "@fortawesome/free-solid-svg-icons";
 
 // components UI kit
 import { MenuButton } from "./customUIkit";
@@ -14,25 +14,55 @@ function Menu() {
       name: 'Home', 
       path: '/',
       icon: faHouse,
-      active: true
+      active: true,
+      type: 'link'
+    },{
+      name: 'Music',
+      path: '/music',
+      icon: faMusic,
+      active: false,
+      type: 'link'
     },{
       name: 'Favorites',
       path: '/favorites',
       icon: faHeart,
-      active: false
+      active: false,
+      type: 'link'
     },{
       name: 'Bookmarks',
       path: '/bookmarks',
       icon: faBookmark,
-      active: false
+      active: false,
+      type: 'link'
+    },{
+      type: 'divider'
+    },{
+      name: 'Play Queue',
+      path: '/queue',
+      icon: faBarsStaggered,
+      active: false,
+      type: 'link'
+    },{
+      name: 'Playerlist',
+      path: '/playerlist',
+      icon: faList,
+      active: false,
+      type: 'link'
     }]);
     
 
   return (
     <ul className="menu p-3 pt-10 gap-3">
-      {menu.map((item, index) => (
-        <MenuButton props={item} key={index} />
-      ))}
+      {menu.map((item, index) => {
+        switch (item.type) {
+          case 'link':
+            return <MenuButton key={index} props={item} />;
+          case 'divider':
+            return <div key={index} className="divider mt-0 mb-0" />;
+          default:
+            return <span>Error</span>;
+        }
+      })}
 
       {/* setting */}
       <li className="mt-auto">
